da6390e0f611266a15f9bafec25c6040940407c6
Mahir Gulzar <mahirgulzar@gmail.com>
2017-12-27 20:23:36 +0200

    Random Trial Test
    Random Rotation Trial Test

---
+++ Assets/Scripts/Offspring.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Offspring : MonoBehaviour {
+
+    public GameObject[] Joints;
+    Quaternion[] rotational_vectors;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        foreach (GameObject joint in Joints)
+        {
+            Quaternion temp = Quaternion.Euler(Random.Range(0,360), 0, Random.Range(0, 360));
+            joint.transform.rotation = temp;
+        }
+
+
+    }
+}
