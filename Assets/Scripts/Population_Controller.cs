85b4e68af9309562c83e8a27801e2fb770b0c46c
Novin Shahroudi <novinsha@gmail.com>
2017-12-30 11:04:23 +0200

    Fix the second (new) approach.
    	- Keep elites (with an eliterate from previous population)
    	- AddNoise to a porportion of the elites (not working yet!)
    	- Cross over elite-elite and elite-non-elite (with specified
    	rate)
    	- Mutation with a given rate
    		- on elites just last 2 joints
    		- on non-elites total joints
    	- some improvements still needed
    	- some fixmes required for the tweaks (dynamic parameters)

--- Assets/Scripts/Population_Controller.cs
+++ Assets/Scripts/Population_Controller.cs
@@ -8,25 +8,25 @@ public class Population_Controller : MonoBehaviour {
     List<RoboticArm> population = new List<RoboticArm>();
     [Header("Tweakers")]
     [Space(15)]
-    public int populationSize = 100;   // number of robotic arms (virtual creatures)
+    public int populationSize = 100;    // number of robotic arms (virtual creatures)
+    public int numOfGenerations = 100;  // number of generations
     [HideInInspector]
-    public int chromosomeLength;    // number of genes (properties of the creature)
-    [Range(0,1)]
-    public float eliteRate;    // number of top best individuals in each generation
-    [Range(0, 1)]
-    public float mutationRate = 0.1f; // chance of getting mutated for each individual 
-    //[Range(0, 0.5f)]
-    //public float crossOverRate;    // number of top best individuals in each generation
+    public int chromosomeLength;        // number of genes (properties of the creature) = chromosomeLength
+    [Range(0, 1f)]
+    public float eliteRate;             // number of top best individuals in each generation
+    [Range(0, 1f)]
+    public float mutationRate = 0.01f;  // chance of getting mutated for each individual 
+    [Range(0, 1f)]
+    public float eliteXOverRate = 0.4f; // number of elite-elite cross over
 
     //[Range(-1,1)]
-    public float noiseRate;
+    public float noiseRate;     // TODO: employ this factor!
 
 
     [Header("References")]
     [Space(15)]
     public GameObject target;   // target for the creature
     public GameObject prefab;   // prefabrication of the robotic arm
-
 
 
     bool updateScene = false;   // we update in delayed intervals for visualization purposes
@@ -46,17 +46,18 @@ public class Population_Controller : MonoBehaviour {
             return;
 
         updateScene = false;
-        if (generationCounter < 100)
+        // TODO: stop optimization when reached at the target!
+        if (generationCounter < numOfGenerations)
         {
             //Debug.Log("Generation: " + generationCounter);
             NewGeneration();
             generationCounter++;
         }
-        else
+        else // This is to update only if we want to show the final result of the optimization!
         {
             //foreach (RoboticArm p in population)
             //{
-            //    p.updateItMan = true;
+            //    p.freeze = false;
             //}
         }
     }
@@ -77,8 +78,9 @@ public class Population_Controller : MonoBehaviour {
 
     void NewGeneration()
     {
+        // find fitness of the population
         population = population.OrderBy(o => o.fitness).ToList();
-        population.Reverse();
+        population.Reverse();   // sort descending
         List<Chromosome> evolvedChromosomes = new List<Chromosome>();
         int eliteSize = (int)(eliteRate * populationSize);
 
@@ -87,7 +89,8 @@ public class Population_Controller : MonoBehaviour {
         {
             if (i % 2 == 0)
             {
-                AddNoise(ref population[i].chromosome, Random.Range(1, chromosomeLength / 2));
+                // FIXME: the noise is biased - turns the endeffector upward (perhaps a gaussian noise is better!)
+                // AddNoise(ref population[i].chromosome, Random.Range(1, chromosomeLength / 2));
             }
             evolvedChromosomes.Add(population[i].chromosome);
         }
@@ -96,13 +99,14 @@ public class Population_Controller : MonoBehaviour {
 
         // cross over elite-elite 
         System.Random rand1 = new System.Random();
-        List<Chromosome> elites = new List<Chromosome>(evolvedChromosomes);
-        for (int i = 0; i < 8; i++)
+        List<Chromosome> elitesAsParent = new List<Chromosome>(evolvedChromosomes); // because so far, evolvedChromosomes contain only elites
+        int elitEliteSize = (int)((eliteSize/2) * eliteXOverRate);
+        for (int i = 0; i < elitEliteSize; i++)
         {
             int parent_1_idx = rand1.Next(0, eliteSize - 1);
             int parent_2_idx = rand1.Next(0, eliteSize - 1);
-            elites.Remove(evolvedChromosomes[parent_1_idx]);
-            elites.Remove(evolvedChromosomes[parent_2_idx]);
+            elitesAsParent.Remove(evolvedChromosomes[parent_1_idx]);
+            elitesAsParent.Remove(evolvedChromosomes[parent_2_idx]);
             Chromosome[] children =
                 Crossover(evolvedChromosomes[parent_1_idx], evolvedChromosomes[parent_2_idx]);
 
@@ -113,56 +117,64 @@ public class Population_Controller : MonoBehaviour {
         // ----------------------------------------------------------------
 
         // cross over elite-non-elite 
-        //System.Random rand2 = new System.Random();
-        //for (int i = 0; i < elites.Count(); i++)
-        //{
-        //    int parent_1_idx = i;
-        //    int parent_2_idx = rand2.Next(eliteSize, population.Count());
-        //    Chromosome[] children =
-        //        Crossover(elites[parent_1_idx], population[parent_2_idx].chromosome);
+        System.Random rand2 = new System.Random();
+        for (int i = 0; i < elitesAsParent.Count()/2; i++)
+        {
+            int parent_1_idx = i;
+            int parent_2_idx = rand2.Next(eliteSize, population.Count());
+            Chromosome[] children =
+                Crossover(elitesAsParent[parent_1_idx], population[parent_2_idx].chromosome);
 
-        //    evolvedChromosomes.Add(children[0]);
-        //    evolvedChromosomes.Add(children[1]);
-        //}
+            evolvedChromosomes.Add(children[0]);
+            evolvedChromosomes.Add(children[1]);
+        }
 
         // ----------------------------------------------------------------
 
         // cross over none-elite-none-elite
-        //System.Random rand3 = new System.Random();
-        //for (int i = 0; i < (populationSize - eliteSize)/2; i++)
-        //{
-        //    int parent_1_idx = rand3.Next(eliteSize, populationSize);
-        //    int parent_2_idx = rand3.Next(eliteSize, populationSize);
-        //    Chromosome[] children =
-        //        Crossover(population[parent_1_idx].chromosome, population[parent_2_idx].chromosome);
+        System.Random rand3 = new System.Random();
+        int nonEliteCrossOverSize = (populationSize - evolvedChromosomes.Count) / 2;
+        for (int i = 0; i < nonEliteCrossOverSize; i++)
+        {
+            int parent_1_idx = rand3.Next(eliteSize, populationSize);
+            int parent_2_idx = rand3.Next(eliteSize, populationSize);
+            Chromosome[] children =
+                Crossover(population[parent_1_idx].chromosome, population[parent_2_idx].chromosome);
 
-        //    evolvedChromosomes.Add(children[0]);
-        //    evolvedChromosomes.Add(children[1]);
-        //}
+            evolvedChromosomes.Add(children[0]);
+            evolvedChromosomes.Add(children[1]);
+        }
 
         // ----------------------------------------------------------------
 
-        // Mutate
-        System.Random rand4 = new System.Random();
-        //int mutationSize = populationSize - evolvedChromosomes.Count();
-        int mutationSize = 10;
-        for (int i = 0; i < mutationSize; i++)
+        // Mutate - this mutation overwrite some of the crossovered individuals as if they were not exiting at all
+        for (int i = 0; i < evolvedChromosomes.Count; i++)
         {
-            int j = rand4.Next(0, eliteSize);
-            evolvedChromosomes.Add(Mutate(population[j].chromosome, 2));
+            float mutationChance = Random.Range(0.0f, 1.0f);
+            if (mutationChance <= mutationRate)
+            {
+                // for elite mutate only one of the last two joints
+                if( i < eliteSize)
+                {
+                    Mutate(evolvedChromosomes[i], 2);
+                }
+                else // mutate all joints for non-elite
+                {
+                    Mutate(evolvedChromosomes[i]);
+                }
+            }
         }
 
+        // Make sure the population size remain same
         if (evolvedChromosomes.Count != populationSize)
         {
             Debug.LogError(populationSize - evolvedChromosomes.Count +
                 " individuals passed to next generation without any alteration!");
         }
 
-        // pass the evolved chromosomes to the next generation population
-        for (int i = 0; i < evolvedChromosomes.Count; i++)
-        {
+        // pass the evolved chromosomes to the next generation
+        for (int i = 0; i < evolvedChromosomes.Count(); i++)
             population[i].chromosome = evolvedChromosomes[i];
-        }
 
         // Reset Joints to initial positions
         ResetToInitial();
@@ -182,7 +194,9 @@ public class Population_Controller : MonoBehaviour {
 
     private Chromosome Mutate(Chromosome chrome, int effectedLastGenes = 1)
     {
-        int i = Random.Range(chromosomeLength-effectedLastGenes, chromosomeLength - 1); 
+        System.Random rand = new System.Random();
+        //int i = Random.Range(chromosomeLength-effectedLastGenes, chromosomeLength - 1); 
+        int i = rand.Next(chromosomeLength - effectedLastGenes, chromosomeLength - 1);
         chrome.genes[i] = Quaternion.Euler(Random.Range(-90, 90), 0, Random.Range(-90, 90));
         return chrome;
     }
